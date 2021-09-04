b9ae3b6be1ae8bdd6837d0fdb39ea363ed26002b
sofiadelano <smdelano@uc.cl>
2021-09-04 18:10:16 -0400

    my awesome first commit

---
+++ entrega0/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from entrega0.users.models import User
+from entrega0.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
